7cbf84a4b4801659af49135da68d3c68ef265953
Jun <neohwj28@gmail.com>
2023-01-29 14:11:16 +0800

    Add word list and guess

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
-import React, { useState } from "react";
+import { useState } from "react";
+import words from "./WordList.json";
+import styles from "./app.module.css";
 
 function App() {
-	const [wordToGuess, setWordToGuess] = useState(0);
+	const [wordToGuess, setWordToGuess] = useState(() => {
+		// Pick a random word from the list
+		return words[Math.floor(Math.random() * words.length)];
+	});
 
-	return <h1>Hi</h1>;
+	const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
+
+	return (
+		<div className={styles.body}>
+			<div className={styles.modal}>Lose Win</div>
+			{/* <HangmanDrawing />
+			<HangmanWord />
+			<Keyboard /> */}
+			<h1>{wordToGuess}</h1>
+		</div>
+	);
 }
 
 export default App;
